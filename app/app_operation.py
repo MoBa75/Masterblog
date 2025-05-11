d53fb49d9533b35e0db58dd2576664cce9aa8edd
MoBa75 <barison@gmx.de>
2025-05-11 19:25:04 +0200

    refined id creation logic

--- app/app_operation.py
+++ app/app_operation.py
@@ -2,7 +2,14 @@ from app.json_storage import get_data
 
 
 def create_new_id():
+    """
+
+    :return:
+    """
     posts = get_data()
-    new_id = posts[-1]['id'] + 1
-    print(new_id)
+    # default value for the first comment
+    new_id = 1
+    id_lst = [int(post["id"]) for post in posts if "id" in post
+              and str(post.get("id", "")).isdigit()]
+    new_id = next(num for num in range(1, len(posts)+2) if num not in id_lst)
     return new_id
